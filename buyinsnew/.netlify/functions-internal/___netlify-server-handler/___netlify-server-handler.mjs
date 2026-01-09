5be7a78a5007cce136584b6d9060f178bc348e96
Rani Ophir <raniop@mac.com>
2026-01-09 22:30:19 +0200

    הזזת ספינר תעודת זהות עוד קצת ימינה (left-6)

--- buyinsnew/.netlify/functions-internal/___netlify-server-handler/___netlify-server-handler.mjs
+++ buyinsnew/.netlify/functions-internal/___netlify-server-handler/___netlify-server-handler.mjs
@@ -2,28 +2,32 @@ import {
   createRequestContext,
   runWithRequestContext,
 } from '/var/task/travelsure-1/buyinsnew/.netlify/dist/run/handlers/request-context.cjs'
-import { getTracer, withActiveSpan } from '/var/task/travelsure-1/buyinsnew/.netlify/dist/run/handlers/tracer.cjs'
+import { getTracer } from '/var/task/travelsure-1/buyinsnew/.netlify/dist/run/handlers/tracer.cjs'
+import tracing from '/var/task/travelsure-1/buyinsnew/.netlify/dist/run/handlers/tracing.js'
 
 process.chdir('/var/task/travelsure-1/buyinsnew')
 
 // Set feature flag for regional blobs
-process.env.USE_REGIONAL_BLOBS = 'true'
+process.env.USE_REGIONAL_BLOBS = 'false'
 
 let cachedHandler
 export default async function (req, context) {
-  const requestContext = createRequestContext(req, context)
+  if (process.env.NETLIFY_OTLP_TRACE_EXPORTER_URL) {
+    tracing.start()
+  }
+
+  const requestContext = createRequestContext(req.headers.get('x-next-debug-logging'))
   const tracer = getTracer()
 
   const handlerResponse = await runWithRequestContext(requestContext, () => {
-    return withActiveSpan(tracer, 'Next.js Server Handler', async (span) => {
-      span?.setAttributes({
+    return tracer.withActiveSpan('Next.js Server Handler', async (span) => {
+      span.setAttributes({
         'account.id': context.account.id,
         'deploy.id': context.deploy.id,
         'request.id': context.requestId,
         'site.id': context.site.id,
         'http.method': req.method,
         'http.target': req.url,
-        isBackgroundRevalidation: requestContext.isBackgroundRevalidation,
         monorepo: true,
         cwd: '/var/task/travelsure-1/buyinsnew',
       })
@@ -31,8 +35,8 @@ export default async function (req, context) {
         const { default: handler } = await import('/var/task/travelsure-1/buyinsnew/.netlify/dist/run/handlers/server.js')
         cachedHandler = handler
       }
-      const response = await cachedHandler(req, context, span, requestContext)
-      span?.setAttributes({
+      const response = await cachedHandler(req, context)
+      span.setAttributes({
         'http.status_code': response.status,
       })
       return response
