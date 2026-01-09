e3598d0844bd81e0a4f565fbc17093f11239e0d7
Rani Ophir <raniop@mac.com>
2026-01-09 17:33:36 +0200

    תיקון: הצגת תאריך כשהשדה לא ריק - class date-has-value, הטקסט dd/mm/yyyy מוסתר רק כשהשדה ריק

--- buyinsnew/app/page.tsx
+++ buyinsnew/app/page.tsx
@@ -225,6 +225,8 @@ function FloatingInput({
           type === "date" && "[&::-webkit-calendar-picker-indicator]:hidden [&::-webkit-calendar-picker-indicator]:appearance-none [&::-webkit-inner-spin-button]:hidden [&::-webkit-outer-spin-button]:hidden",
           // עבור תאריך - הסתר placeholder לחלוטין תמיד
           type === "date" && "[&::placeholder]:opacity-0 [&::placeholder]:hidden [&::-webkit-input-placeholder]:opacity-0 [&::-webkit-input-placeholder]:hidden [&::-moz-placeholder]:opacity-0 [&::-moz-placeholder]:hidden [&:-ms-input-placeholder]:opacity-0 [&:-ms-input-placeholder]:hidden",
+          // הוסף class כשהשדה תאריך יש לו ערך
+          type === "date" && hasValue && "date-has-value",
           className
         )}
         {...(type === "date" ? { placeholder: "" } : { placeholder: shouldFloat ? undefined : (props.placeholder || "") })}
